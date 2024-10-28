bade9acac344921a5cae8f6d9b043182fe1835a8
AnshikaGoel <11402anshikagoyal@gmail.com>
2024-10-28 15:24:16 -0700

    all search function done

--- lib/ItemsLists/item_data.dart
+++ lib/ItemsLists/item_data.dart
@@ -5,8 +5,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 final itemDataProvider = Provider<List<Item>>((ref) => [
       Item(name: 'Grocery', imagePath: 'assets/images/grocery.jpg'),
       Item(name: 'Medicine', imagePath: 'assets/images/medicines.jpg'),
-      Item(name: 'Appliances', imagePath: 'assets/images/appliances.png'),
-      Item(name: 'Flights', imagePath: 'assets/images/flights.png'),
-      Item(name: 'Hotels', imagePath: 'assets/images/hotels.png'),
-      Item(name: 'Insurances', imagePath: 'assets/images/insurance.png'),
+      Item(name: 'Appliances', imagePath: 'assets/images/appliances.jpg'),
+      Item(name: 'Flights', imagePath: 'assets/images/flight.webp'),
+      Item(name: 'Hotels', imagePath: 'assets/images/hotel.jpeg'),
+      Item(name: 'Insurances', imagePath: 'assets/images/insurance.jpeg'),
     ]);
